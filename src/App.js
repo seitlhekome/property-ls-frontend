f29c2ca97109c12e6422e1285585769fa94253c0
Seitlheko <seitlheko.me@gmail.com>
2026-05-06 21:36:23 +0200

    Fix Google Analytics setup

--- src/App.js
+++ src/App.js
@@ -51,20 +51,20 @@ export default function App() {
   });
 
   useEffect(() => {
-    if (GA_MEASUREMENT_ID && GA_MEASUREMENT_ID !== "G-D32DK9BLQ7") {
-      ReactGA.initialize(GA_MEASUREMENT_ID);
-    }
-  }, []);
+  if (GA_MEASUREMENT_ID) {
+    ReactGA.initialize(GA_MEASUREMENT_ID);
+  }
+}, []);
 
-  useEffect(() => {
-    if (GA_MEASUREMENT_ID && GA_MEASUREMENT_ID !== "G-D32DK9BLQ7") {
-      ReactGA.send({
-        hitType: "pageview",
-        page: location.pathname + location.search,
-        title: document.title,
-      });
-    }
-  }, [location]);
+useEffect(() => {
+  if (GA_MEASUREMENT_ID) {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search,
+      title: document.title,
+    });
+  }
+}, [location]);
 
   const initialPropertyState = useMemo(
     () => ({
